3aad7aa0040eb6dac882cfa1691d959ee8a4da5e
Elenterius <w.elenterius@gmail.com>
2025-08-27 17:29:52 +0200

    fix: fix big flesh blobs not being able to attack other mobs due to flawed attack reach calculations

--- src/main/java/com/github/elenterius/biomancy/entity/mob/fleshblob/FleshBlobAttackGoal.java
+++ src/main/java/com/github/elenterius/biomancy/entity/mob/fleshblob/FleshBlobAttackGoal.java
@@ -1,5 +1,6 @@
 package com.github.elenterius.biomancy.entity.mob.fleshblob;
 
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
 
@@ -9,10 +10,16 @@ class FleshBlobAttackGoal extends MeleeAttackGoal {
 		super(mob, speed, true);
 	}
 
+	private double getSizePct() {
+		return (double) (((FleshBlob) mob).getBlobSize() - FleshBlob.MIN_SIZE) / (double) (FleshBlob.MAX_SIZE - FleshBlob.MIN_SIZE);
+	}
+
 	@Override
 	protected double getAttackReachSqr(LivingEntity attackTarget) {
-		float radius = mob.getBbWidth() * 0.5f;
-		return radius * radius + 0.75f + attackTarget.getBbWidth();
+		double actualAttackReach = Mth.lerp(getSizePct(), 0.25d, 0.75d); //measured from the faces of the AABB and not the origin/positon of the mob
+		double inflatedHalfWidth = (mob.getBbWidth() + attackTarget.getBbWidth()) * 0.5d; //similar to minkowski sum and testing if the target position is inside the inflated AABB
+		double radius = inflatedHalfWidth + actualAttackReach;
+		return radius * radius + 1e-7d;
 	}
 
 }
