e527a74defee951c4d33a415c8349ed35644a44d
Elenterius <w.elenterius@gmail.com>
2025-11-29 00:44:05 +0100

    feat: render the ravenous claws with player arms

--- src/main/java/com/github/elenterius/biomancy/client/render/item/ravenousclaws/RavenousClawsRenderer.java
+++ src/main/java/com/github/elenterius/biomancy/client/render/item/ravenousclaws/RavenousClawsRenderer.java
@@ -1,9 +1,9 @@
 package com.github.elenterius.biomancy.client.render.item.ravenousclaws;
 
+import com.github.elenterius.biomancy.client.render.item.GeoItemWithArmRenderer;
 import com.github.elenterius.biomancy.item.weapon.RavenousClawsItem;
-import software.bernie.geckolib.renderer.GeoItemRenderer;
 
-public class RavenousClawsRenderer extends GeoItemRenderer<RavenousClawsItem> {
+public class RavenousClawsRenderer extends GeoItemWithArmRenderer<RavenousClawsItem> {
 
 	public RavenousClawsRenderer() {
 		super(new RavenousClawsModel());
