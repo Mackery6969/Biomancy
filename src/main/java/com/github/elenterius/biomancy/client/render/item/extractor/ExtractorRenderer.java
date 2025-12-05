1f1586635a19e4d613bae301832dd6422d0bb74f
Elenterius <w.elenterius@gmail.com>
2025-12-05 22:48:47 +0100

    feat: render the extractor with player arms

--- src/main/java/com/github/elenterius/biomancy/client/render/item/extractor/ExtractorRenderer.java
+++ src/main/java/com/github/elenterius/biomancy/client/render/item/extractor/ExtractorRenderer.java
@@ -1,10 +1,12 @@
 package com.github.elenterius.biomancy.client.render.item.extractor;
 
+import com.github.elenterius.biomancy.client.render.item.GeoItemWithArmRenderer;
 import com.github.elenterius.biomancy.item.extractor.ExtractorItem;
-import software.bernie.geckolib.renderer.GeoItemRenderer;
 
-public class ExtractorRenderer extends GeoItemRenderer<ExtractorItem> {
+public class ExtractorRenderer extends GeoItemWithArmRenderer<ExtractorItem> {
+
 	public ExtractorRenderer() {
 		super(new ExtractorModel());
 	}
+
 }
