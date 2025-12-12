48cfdbf624e84079fc73a689576b0cf475086d2e
Elenterius <w.elenterius@gmail.com>
2025-12-12 14:34:38 +0100

    docs(wiki): fix wiki recipe format

--- src/datagen/java/com/github/elenterius/biomancy/datagen/recipes/WikiRecipesProvider.java
+++ src/datagen/java/com/github/elenterius/biomancy/datagen/recipes/WikiRecipesProvider.java
@@ -2,10 +2,14 @@ package com.github.elenterius.biomancy.datagen.recipes;
 
 import com.github.elenterius.biomancy.datagen.recipes.builder.WikiRecipe;
 import com.google.common.collect.Sets;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import net.minecraft.data.CachedOutput;
 import net.minecraft.data.DataProvider;
 import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 
@@ -49,12 +53,53 @@ public class WikiRecipesProvider implements DataProvider {
 					list.add(DataProvider.saveStable(output, wikiRecipe.serializeWikiRecipe(), resolveJsonPath(finishedRecipe.getId())));
 				}
 				else if (SUPPORTED_VANILLA_RECIPES.contains(finishedRecipe.getType())) {
-					list.add(DataProvider.saveStable(output, finishedRecipe.serializeRecipe(), resolveJsonPath(finishedRecipe.getId())));
+					list.add(DataProvider.saveStable(output, serializeTransformVanilla(finishedRecipe), resolveJsonPath(finishedRecipe.getId())));
 				}
 			}
 		});
 
 		return CompletableFuture.allOf(list.toArray(CompletableFuture[]::new));
+	}
+
+	private JsonObject serializeTransformVanilla(FinishedRecipe finishedRecipe) {
+		JsonObject json = finishedRecipe.serializeRecipe();
+
+		RecipeSerializer<?> type = finishedRecipe.getType();
+
+		if (SUPPORTED_VANILLA_RECIPES.contains(type)) {
+
+			if (type != RecipeSerializer.STONECUTTER && type != RecipeSerializer.BLASTING_RECIPE) {
+				if (json.has("ingredient")) {
+					WikiRecipe.transform(json.get("ingredient"));
+				}
+			}
+
+//			if (json.has("ingredients")) {
+//				JsonArray ingredients = GsonHelper.getAsJsonArray(json, "ingredients");
+//				for (int i = 0; i < ingredients.size(); i++) {
+//					JsonElement jsonElement = ingredients.get(i);
+//					WikiRecipe.transform(jsonElement);
+////					if (WikiRecipe.transform(jsonElement) instanceof JsonObject obj) {
+////						String id = GsonHelper.getAsString(obj, "id");
+////						ingredients.set(i, new JsonPrimitive(id));
+////					}
+//				}
+//			}
+
+			if (json.has("result")) {
+				JsonElement result = json.get("result");
+				if (result instanceof JsonObject) {
+					WikiRecipe.transform(result);
+				}
+				else {
+					JsonObject resultOverride = new JsonObject();
+					resultOverride.addProperty("id", GsonHelper.getAsString(json, "result"));
+					json.add("result", resultOverride);
+				}
+			}
+		}
+
+		return json;
 	}
 
 	@Override
--- src/main/java/com/github/elenterius/biomancy/datagen/recipes/builder/WikiRecipe.java
+++ src/main/java/com/github/elenterius/biomancy/datagen/recipes/builder/WikiRecipe.java
@@ -4,6 +4,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.util.GsonHelper;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
@@ -21,13 +22,50 @@ public interface WikiRecipe extends FinishedRecipe {
 		AtomicInteger o = new AtomicInteger();
 
 		serializeWikiRecipeData(
-				in -> input.add(Integer.toString(i.getAndIncrement()), in),
-				out -> output.add(Integer.toString(o.getAndIncrement()), out)
+				in -> input.add(Integer.toString(i.getAndIncrement()), transform(in)),
+				out -> output.add(Integer.toString(o.getAndIncrement()), transform(out))
 		);
 
 		root.add("input", input);
 		root.add("output", output);
 		return root;
+	}
+
+	static JsonElement transform(JsonElement json) {
+		if (json instanceof JsonObject jsonObject) {
+			if (jsonObject.has("item")) {
+				if (jsonObject.has("id")) throw new RuntimeException("JSON already contains 'id' member!");
+
+				JsonElement item = jsonObject.get("item");
+				jsonObject.remove("item");
+
+				if (item instanceof JsonObject) {
+					if (jsonObject.has("type")) {
+						String type = GsonHelper.getAsString(jsonObject, "type");
+						if (type.equals("biomancy:essence")) {
+							jsonObject.remove("type");
+							jsonObject.addProperty("id", type);
+							JsonElement nbt = jsonObject.get("predicate_tag");
+							jsonObject.remove("predicate_tag");
+							jsonObject.add("nbt", nbt);
+						}
+						else throw new RuntimeException("Unhandled ingredient type: " + type);
+					}
+					else throw new RuntimeException("Unknown item type: " + item);
+				}
+				else {
+					jsonObject.add("id", item);
+				}
+			}
+			else if (jsonObject.has("tag")) {
+				if (jsonObject.has("id")) throw new RuntimeException("JSON already contains 'id' member!");
+
+				String tag = GsonHelper.getAsString(jsonObject, "tag");
+				jsonObject.remove("tag");
+				jsonObject.addProperty("id", "#" + tag);
+			}
+		}
+		return json;
 	}
 
 	void serializeWikiRecipeData(Consumer<JsonElement> input, Consumer<JsonElement> output);
